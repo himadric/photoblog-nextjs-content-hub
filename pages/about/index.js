15795b4935efc851d2899f837092f7a8e01b72c7
Himadri Chakrabarti <himadric@yahoo.com>
2021-07-31 08:35:23 -0400

    Completed About page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,23 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import getConfig from 'next/config'
 import GetContentHubClient from '../../Helpers/GetContentHubClient'
 import MessageBlock from '../../components/MessageBlock'
-import OAuthPasswordGrant from "@sitecore/sc-contenthub-webclient-sdk/dist/authentication/oauth-password-grant";
-import { ContentHubClient } from "@sitecore/sc-contenthub-webclient-sdk/dist/clients/content-hub-client";
 import { PropertyQueryFilter } from "@sitecore/sc-contenthub-webclient-sdk/dist/contracts/querying/filters/property-query-filter";
 import { FilterDataType } from "@sitecore/sc-contenthub-webclient-sdk/dist/contracts/querying/filters/filter-data-type";
 import { ComparisonOperator } from "@sitecore/sc-contenthub-webclient-sdk/dist/contracts/querying/filters/comparison-operator";
 import { Query } from "@sitecore/sc-contenthub-webclient-sdk/dist/contracts/querying/query";
-
-const MESSAGE = {
-  "heading": "Photographer",
-  "message": `Thanks for visiting my photography blog website. I am a semi professional photographer with avid interest
-  in taking photos, photographers, camera equipments and work of camera designers. I like to travel and take
-  photos. I have created this blog to share my travel experience and share my love about camera, work of famous
-  photographers and camera designers. If you would like see my work please visit my 
-  <a href="https://www.himadriphoto.com/" aria-label="Photography site" target="_blank">photography site</a>.`
-}
 
 export default function About(props) {
   return (
@@ -30,9 +18,9 @@ export default function About(props) {
 
         <div className='container'>
         <MessageBlock
-          heading={props.message.heading}
-          message ={props.message.message} />
-          <Image src="/img/himadri.jpg" width='1038' height='692' alt="Himadri Chakrabarti" />
+          heading={props.message.title}
+          message ={props.message.body} />
+          <Image src={props.message.image} width='1038' height='692' alt="Himadri Chakrabarti" />
         </div>
     </>
   )
@@ -47,20 +35,21 @@ export async function getStaticProps() {
       property: "Content.Name",
       value: "About",
       dataType: FilterDataType.String
-  });
+    });
   
-  var query = new Query({
+    var query = new Query({
       filter: propertyQueryFilter
-  });
+    });
     var content = await client.querying.singleAsync(query);
-    //const aggregateResource = await client.aggregates.getAsync("ContentCollection", 30491);
-    //var content = await client.aggregates.getAsync("Content", 30500);;
-    console.log(content.getProperty("Blog_Body").getValue());
-  }
-  return {
-    props: {
-      message: MESSAGE
-    },
-    revalidate: 86400
+    return {
+      props: {
+        message: {
+          title: content.getProperty("Blog_Title").getValue(),
+          body: content.getProperty("Blog_Body").getValue(),
+          image: content.getProperty("CoverImageLink").getValue()
+        }
+      },
+      revalidate: 86400
+    }
   }
 }
